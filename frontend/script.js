8dd461008eba2707071078c26f8ea6c2423ca7a6
AlexLee09 <alexandrekmlee@gmail.com>
2024-03-05 17:42:30 +0800

    Replace frontend with reactjs

--- frontend/script.js
+++
@@ -1,8 +0,0 @@
-async function mainFunction() {
-    const request = await fetch("http://localhost:3001/");
-    const data = await request.json();
-    console.log(data);
-    
-    const text = document.getElementById('text');
-    text.innerHTML = data.key
-}
