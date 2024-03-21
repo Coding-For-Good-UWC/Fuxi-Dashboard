74396679800c720ed731a64d63688d9cd8711e9d
AlexLee09 <alexandrekmlee@gmail.com>
2024-03-21 18:45:25 +0800

    Basic Css for Login Page

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,57 +1,59 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import classes from './LoginPage.module.css';
 
-function LoginPage()
-{
+function LoginPage() {
     const navigate = useNavigate();
 
-    const [username, setUsername] = useState(""); 
-    const [password, setPassword] = useState(""); 
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
-    const returnToLanding = () =>
-    {
+    const returnToLanding = () => {
         navigate("/");
     }
 
-    const handleLogin = async (event) => 
-    {
-        event.preventDefault(); 
-        
+    const handleLogin = async (event) => {
+        event.preventDefault();
+
         const dataToSend = {
-            username: username, 
+            username: username,
             password: password
         }
 
         const response = await fetch("http://localhost:3001/user/new",
-        {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(dataToSend)
-        })
+            {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(dataToSend)
+            })
 
-        const data = await response.json(); 
+        const data = await response.json();
 
-        console.log (data);
+        console.log(data);
     }
 
-    return(
-        <div>
-            <h1>Login</h1>
+    return (
+        <div className={classes.main}>
+            <h1>Project FUXI</h1>
+            <h2>Tapping into the power of music<br/>for Persons with Dementia</h2>
             <form onSubmit={(event) => handleLogin(event)}>
                 <p>Username:</p>
-                <input 
+                <input
                     type="text"
                     placeholder='username'
                     onChange={(event) => setUsername(event.target.value)}>
                 </input>
                 <p>Password:</p>
-                <input 
+                <input
                     type="password"
                     placeholder='password'
-                    onChange={(event) => setPassword(event.target.value)}>    
+                    onChange={(event) => setPassword(event.target.value)}>
                 </input>
-                <input type="submit" value="Login"></input>
-                <button onClick = {returnToLanding}>Go Back</button>
+                <div className={classes.buttons}>
+                    <input type="submit" value="Login" className={classes.logIn}></input>
+                    <input type="submit" value="Sign Up" className={classes.signUp}></input>
+                    {/* <button onClick = {returnToLanding}>Go Back</button> */}
+                </div>
             </form>
         </div>
     )
