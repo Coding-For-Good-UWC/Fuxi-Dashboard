2a39e49ac3fbede8ee072f4fb77b054cb5537da7
AlexLee09 <alexandrekmlee@gmail.com>
2024-04-10 23:18:41 +0800

    Divs and classes of navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,8 +1,8 @@
 import React from 'react'; 
 import { useNavigate } from 'react-router-dom';
 import classes from './Navbar.module.css';
-import hamburgerIcon from '../assets/hamburger.png'
-import profilePicture from '../assets/account.png'
+import menu from '../assets/hamburger.png'
+import pfp from '../assets/account.png'
 
 function Navbar ()
 {
@@ -10,11 +10,13 @@ function Navbar ()
 
     return (
         <div className = {classes.container}>
-            <h1>Fuxi Dashboard</h1>
-            <h2>Tapping into the power of music for Persons with Dementia</h2>
-            <img src = {hamburgerIcon}/>
-            <h3>Username</h3>
-            <img src = {profilePicture}/>
+            <h1 className = {classes.title}>Fuxi Dashboard</h1>
+            <h2 className = {classes.subtitle}>Tapping into the power of music for Persons with Dementia</h2>
+            <img className = {menu} src = {menu}/>
+            <div className = {classes.usernamePfp}>
+                <h3 className = {classes.usernameText}>Username</h3>
+                <img className = {classes.pfp} src = {pfp}/>
+            </div>
 
         </div>
     )
