0cc4f499149b4f0f1e61f924984eeab4a67a9503
AlexLee09 <alexandrekmlee@gmail.com>
2024-04-11 12:31:12 +0800

    Navbar set up

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import classes from './Navbar.module.css';
 import menu from '../assets/hamburger.png'
 import pfp from '../assets/account.png'
+import * as FaIcons from "react-icons/fa"; 
 
 function Navbar ()
 {
@@ -10,12 +11,16 @@ function Navbar ()
 
     return (
         <div className = {classes.container}>
-            <h1 className = {classes.title}>Fuxi Dashboard</h1>
-            <h2 className = {classes.subtitle}>Tapping into the power of music for Persons with Dementia</h2>
-            <img className = {menu} src = {menu}/>
-            <div className = {classes.usernamePfp}>
+            <div className = {classes.leftContainer}>
+                <FaIcons.FaBars className = {classes.menu}/>
+            </div>
+            <div className = {classes.centerContainer}>
+                <h1 className = {classes.title}>Fuxi Dashboard</h1>
+                <h2 className = {classes.subtitle}>Tapping into the power of music for Persons with Dementia</h2>
+            </div>
+            <div className = {classes.rightContainer}>
                 <h3 className = {classes.usernameText}>Username</h3>
-                <img className = {classes.pfp} src = {pfp}/>
+                <FaIcons.FaUser className = {classes.pfp}/>
             </div>
 
         </div>
