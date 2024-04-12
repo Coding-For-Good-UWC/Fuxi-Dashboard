95a059f8d7e84bea7fdd3b36d01d964c57ed89c3
AlexLee09 <alexandrekmlee@gmail.com>
2024-04-12 11:03:43 +0800

    Adjustments to Navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -7,19 +7,29 @@ import * as FaIcons from "react-icons/fa";
 
 function Navbar ()
 {
+
+    const navigate = useNavigate();
+    const handleLogin = () => 
+    {
+        navigate("/login")
+    }
+    const toDashboardPage = () => 
+    {
+        navigate("/dashboard")
+    }
     
 
     return (
         <div className = {classes.container}>
             <div className = {classes.leftContainer}>
                 <FaIcons.FaBars className = {classes.menu}/>
+                <h1 onClick = {toDashboardPage} className = {classes.title}>Fuxi Dashboard</h1>
+                {/* <h2 className = {classes.subtitle}>Tapping into the power of music for Persons with Dementia</h2> */}
             </div>
-            <div className = {classes.centerContainer}>
-                <h1 className = {classes.title}>Fuxi Dashboard</h1>
-                <h2 className = {classes.subtitle}>Tapping into the power of music for Persons with Dementia</h2>
-            </div>
+            
+       
             <div className = {classes.rightContainer}>
-                <h3 className = {classes.usernameText}>Username</h3>
+                <h3 onClick = {handleLogin} className = {classes.usernameText}>Login/Signup</h3>
                 <FaIcons.FaUser className = {classes.pfp}/>
             </div>
 
