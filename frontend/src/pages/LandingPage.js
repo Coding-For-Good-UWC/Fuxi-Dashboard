c1b81f40c3f1183be1f2cb2cf80d151230b44610
AlexLee09 <alexandrekmlee@gmail.com>
2024-04-12 12:55:27 +0800

    Basic Landing Page setup

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -1,6 +1,8 @@
 import React from 'react'; 
 import { useNavigate } from 'react-router-dom';
-import classes from './LandingPage.module.css'; 
+import classes from './LandingPage.module.css';
+import * as FaIcons from "react-icons/fa";  
+import Man from '../assets/Man.png'
 
 function LandingPage () 
 {
@@ -39,16 +41,23 @@ function LandingPage ()
     }
 
     return (
-        <div>
-            <h1 className={classes.title}>Landing Page</h1>
-            <button onClick={handleLogin}>Login</button>
-            <button onClick = {handleDashboard}>Dashboard Page</button>
-            <button onClick = {handleSongManagement}>Manage Songs</button>
-            <button onClick = {handleSongPreview}>Preview Songs</button>
-            <button onClick = {handleSongUpload}>Upload Songs</button>
-            <button onClick = {handleUserManage}>Manage Users</button>
+        <div className = {classes.container}>
+            <div className = {classes.leftContainer}>
+                <h1 className={classes.title}>Tapping into the power <br></br>of music for Persons<br></br> with Dementia</h1>
+                <h2 className = {classes.subtitle}>FUXI is an application made to assist Persons with <br></br>Dementia and those who care for them by tapping into the <br></br>power of music in improving wellbeing and engagement.</h2> 
+                <button className = {classes.getStartedButton}>Get Started</button>
+            </div>
+            <div className = {classes.rightContainer}>
+                <img className = {classes.man} src = {Man}></img>
+            </div>
         </div>
     )
 }
 
 export default LandingPage; 
+{/* <button onClick={handleLogin}>Login</button>
+<button onClick = {handleDashboard}>Dashboard Page</button>
+<button onClick = {handleSongManagement}>Manage Songs</button>
+<button onClick = {handleSongPreview}>Preview Songs</button>
+<button onClick = {handleSongUpload}>Upload Songs</button>
+<button onClick = {handleUserManage}>Manage Users</button> */}
